e08154128c752cfc6e010352f4672e04b2328450
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-12 00:45:41 -0600

    Update user profile api

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import User from "../models/user";
+
 const createCurrentUser = async (req: Request, res: Response): Promise<void> => {
     try {
         const { auth0Id } = req.body;
-        const existingUser = await User.findOne({ auth0Id })
+        const existingUser = await User.findOne({ auth0Id });
 
         if(existingUser) {
             res.status(200).send();
-            return
-        }
+            return;
+        };
 
         const newUser = new User(req.body);
         await newUser.save();
@@ -20,6 +21,30 @@ const createCurrentUser = async (req: Request, res: Response): Promise<void> => 
     }
 };
 
+const updateCurrentUser = async (req: Request, res: Response) => {
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+
+        await user.save();
+
+        res.send(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error updating user" });
+    }
+};
+
 export default {
-    createCurrentUser
+    createCurrentUser,
+    updateCurrentUser
 };
